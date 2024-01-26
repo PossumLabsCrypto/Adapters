849b6778f7c74958bafe55806f51881c3b71ba9c
0xmahdirostami <rostamimahdi1997@gmail.com>
2024-01-26 13:39:50 +0330

    updatePortalInterface

--- src/interfaces/IHlpPortal.sol
+++ src/interfaces/IHlpPortal.sol
@@ -6,8 +6,6 @@ interface IHlpPortal {
 
     function unstake(uint256 _amount) external;
 
-    function forceUnstakeAll() external;
-
     function buyPortalEnergy(
         uint256 _amountInput,
         uint256 _minReceived,
@@ -37,26 +35,6 @@ interface IHlpPortal {
         address _recipient,
         uint256 _amount
     ) external;
-
-    function getUpdateAccount(
-        address _user,
-        uint256 _amount
-    )
-        external
-        view
-        returns (
-            address user,
-            uint256 lastUpdateTime,
-            uint256 lastMaxLockDuration,
-            uint256 stakedBalance,
-            uint256 maxStakeDebt,
-            uint256 portalEnergy,
-            uint256 availableToWithdraw
-        );
-
-    function quoteforceUnstakeAll(
-        address _user
-    ) external view returns (uint256 portalEnergyTokenToBurn);
 
     function maxLockDuration() external view returns (uint256 maxLockDuration);
 
