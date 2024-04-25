93b04d5f355ab80a9be3793eac8b5a1a6010348c
PossumLabsCrypto <alex@possumfinance.com>
2024-04-25 22:52:48 +0200

    fix #69

--- src/interfaces/IAdapterV1.sol
+++ src/interfaces/IAdapterV1.sol
@@ -11,12 +11,16 @@ struct Account {
 
 struct SwapData {
     address receiver;
-    uint256 psmAmount;
+    uint256 amountReceived;
     bytes actionData;
 }
 
 interface IAdapterV1 {
     function PORTAL() external view returns (address PORTAL);
+
+    function acceptMigrationDestination() external;
+
+    function executeMigration() external;
 
     function migrateStake(address _user)
         external
@@ -28,73 +32,42 @@ interface IAdapterV1 {
             uint256 portalEnergy
         );
 
-    function stake(address _receiver, uint256 _amount) external;
+    function getUpdateAccount(address _user, uint256 _amount, bool _isPositiveAmount)
+        external
+        view
+        returns (
+            uint256 lastUpdateTime,
+            uint256 lastMaxLockDuration,
+            uint256 stakedBalance,
+            uint256 maxStakeDebt,
+            uint256 portalEnergy,
+            uint256 availableToWithdraw,
+            uint256 portalEnergyTokensRequired
+        );
+
+    function stake(uint256 _amount) external;
 
     function unstake(uint256 _amount) external;
 
-    function mintPortalEnergyToken(address _recipient, uint256 _amount) external;
+    function buyPortalEnergy(address _recipient, uint256 _amountInputPSM, uint256 _minReceived, uint256 _deadline)
+        external;
+
+    function sellPortalEnergy(
+        address payable _recipient,
+        uint256 _amountInputPE,
+        uint256 _minReceived,
+        uint256 _deadline,
+        uint256 _mode,
+        bytes calldata _actionData,
+        uint256 _minPSMForLiquidiy,
+        uint256 _minWethForLiquidiy
+    ) external;
 
     function burnPortalEnergyToken(address _recipient, uint256 _amount) external;
 
-    function buyPortalEnergy(address _user, uint256 _amount, uint256 _minReceived, uint256 _deadline) external;
+    function mintPortalEnergyToken(address _recipient, uint256 _amount) external;
 
-    function sellPortalEnergy(
-        address payable _receiver,
-        uint256 _amount,
-        uint256 _minReceivedPSM,
-        uint256 _deadline,
-        uint256 _mode,
-        bytes calldata _actionData
-    ) external;
+    function increaseAllowances() external;
 
-    function addLiquidity(
-        address _receiver,
-        uint256 amountPSMDesired,
-        uint256 amountWETHDesired,
-        uint256 amountPSMMin,
-        uint256 amountWETHMin,
-        uint256 _deadline
-    ) external returns (uint256 amountPSM, uint256 amountWETH, uint256 liquidity);
-
-    function addLiquidityETH(
-        address _receiver,
-        uint256 _amountPSMDesired,
-        uint256 _amountPSMMin,
-        uint256 _amountETHMin,
-        uint256 _deadline
-    ) external payable returns (uint256 amountPSM, uint256 amountETH, uint256 liquidity);
-
-    function removeLiquidity(
-        address _receiver,
-        uint256 _liquidity,
-        uint256 _amountPSMMin,
-        uint256 _amountWETHMin,
-        uint256 _deadline
-    ) external returns (uint256 amountPSM, uint256 amountWETH);
-
-    function removeLiquidityETH(
-        address _receiver,
-        uint256 _liquidity,
-        uint256 _amountPSMMin,
-        uint256 _amountETHMin,
-        uint256 _deadline
-    ) external returns (uint256 amountPSM, uint256 amountETH);
-
-    function getUpdateAccount(address _user, uint256 _amount)
-        external
-        view
-        returns (address, uint256, uint256, uint256, uint256, uint256, uint256);
-
-    function quoteforceUnstakeAll(address _user) external view returns (uint256 portalEnergyTokenToBurn);
-
-    function quoteBuyPortalEnergy(uint256 _amountInput) external view returns (uint256);
-
-    function quoteSellPortalEnergy(uint256 _amountInput) external view returns (uint256);
-
-    function quoteAddLiquidity(uint256 _amountADesired, uint256 _amountBDesired)
-        external
-        view
-        returns (uint256 _amountPSMDesired, uint256 _amountWETHDesired, uint256 liquidity);
-
-    function quoteRemoveLiquidity(uint256 _liquidity) external view returns (uint256 amountA, uint256 amountB);
+    function salvageToken(address _token) external;
 }
